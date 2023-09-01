1bda67815c8bf62b58964fd16d89650316d05439
kavrad <kavyaht39@gmail.com>
2023-09-01 14:29:29 +0530

    Change title component little bit

---
+++ src/components/TitleComponent.js
@@ -0,0 +1,10 @@
+function Title(props){
+  const {className}=props
+  return (
+    <div className={className}>
+        <p className="text-white text-center capitalize underline text-3xl">Increment And Decrement</p>
+    </div>
+  );
+}
+
+export default Title;
